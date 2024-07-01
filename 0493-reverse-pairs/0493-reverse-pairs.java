4629b48034136fde72faa8913cd720eace6a3385
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-07-01 13:35:03 +0530

    Time: 40 ms (94.45%), Space: 55.5 MB (9.04%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -0,0 +1,61 @@
+class Solution {
+    // public int reversePairs(int[] nums) {
+//      int n=nums.length;
+//         int    count=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 // if(nums[i]>2*nums[j]){
+//                     if ((long)arr[i] > (long)2 * arr[j])   
+//                     count+=1;
+//                 }
+//             }
+//         }
+        
+//         return count;
+        
+         int res = 0;
+    public int reversePairs(int[] nums) {
+        mergeSort(nums,0,nums.length-1);
+        return res;
+    }
+    void mergeSort(int nums[], int l, int r){
+        if(l == r) return;
+        int mid = (l + r)/2;
+        mergeSort(nums,l,mid);
+        mergeSort(nums,mid+1,r);
+        merge(nums,l,mid,r);
+    }
+    void merge(int nums[], int l, int mid, int r){
+        int temp[] = new int[r - l + 1];
+        int i = l, j = mid + 1, k = 0;
+        while(i <= mid && j <= r){
+            while(i <= mid && (long)nums[i] <= 2*(long)nums[j]){
+                i++;
+            }
+            res += mid - i + 1;
+            j++;
+        }
+        i = l;
+        j = mid + 1;
+        while(i <= mid && j <= r){
+            if(nums[i] <= nums[j]){
+                temp[k++] = nums[i++];
+            }
+            else{
+                temp[k++] =  nums[j++];
+            }
+        }
+        while(i <= mid){
+            temp[k++] = nums[i++];
+        }
+        while(j <= r){
+            temp[k++] = nums[j++];
+        }
+        k = 0;
+        for(k = 0; k < temp.length; k++){
+            nums[l + k] = temp[k];
+        }
+    }
+}
+    // }
+// }
