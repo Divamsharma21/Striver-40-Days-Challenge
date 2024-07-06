29e4be1488325816d046b797c62a790c5309efe2
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-07-06 07:59:48 +0530

    Time: 3 ms (45.21%), Space: 45.4 MB (61.15%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -1,24 +1,37 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int count=0;
-        int n=nums.length;
-              int cout=0;
-              if(nums[0]==1){
-                  cout++;
-                  count++;
-              }
+//         int count=0;
+//         int n=nums.length;
+//               int cout=0;
+//               if(nums[0]==1){
+//                   cout++;
+//                   count++;
+//               }
               
-        for(int i=1;i<n;i++){
-            if(nums[i]==1){
-                count++;
-                cout=Math.max(cout,count);
+//         for(int i=1;i<n;i++){
+//             if(nums[i]==1){
+//                 count++;
+//                 cout=Math.max(cout,count);
 
-            }
-            else{
-                count=0;
+//             }
+//             else{
+//                 count=0;
+//             }
+
+//         }
+//             return cout;
+        
+          int cnt = 0;
+        int maxi = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                cnt++;
+            } else {
+                cnt = 0;
             }
 
+            maxi = Math.max(maxi, cnt);
         }
-            return cout;
+        return maxi;
     }
 }
