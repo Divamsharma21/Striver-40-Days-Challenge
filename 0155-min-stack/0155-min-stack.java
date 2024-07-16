5032b8e9f2323e5e0ed62886ecab4524cf24beab
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-07-16 14:04:49 +0530

    Time: 4 ms (95.64%), Space: 44.7 MB (46.60%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -35,49 +35,22 @@
 
 
 class MinStack {
-    Stack < Long > st = new Stack < Long > ();
-    Long mini;
-
-    /** initialize your data structure here. */
-    public MinStack() {
-        mini = Long.MAX_VALUE;
-    }
-
-    public void push(int value) {
-        Long val = Long.valueOf(value);
-        if (st.isEmpty()) {
-            mini = val;
-            st.push(val);
-        } else {
-            if (val < mini) {
-                st.push(2 * val - mini);
-                mini = val;
-            } else {
-                st.push(val);
-            }
-        }
-    }
-
-    public void pop() {
-        if (st.isEmpty()) return;
-
-        Long val = st.pop();
-        if (val < mini) {
-            mini = 2 * mini - val;
-        }
-    }
-
-    public int top() {
-        Long val = st.peek();
-        if (val < mini) {
-            return mini.intValue();
-        }
-        return val.intValue();
-    }
-
-    public int getMin() {
-        return mini.intValue();
-    }
+    Stack<Integer> stack=new Stack<>();
+int min=Integer.MAX_VALUE;
+public void push(int x) {
+    if(x<=min) {stack.push(min); min=x;}
+    stack.push(x);
+}
+public void pop() {
+    if(stack.peek()==min){ stack.pop(); min=stack.pop(); }
+    else stack.pop();
+}
+public int top() {
+    return stack.peek();
+}
+public int getMin() {
+    return min;
+}
 }
 
 /**
