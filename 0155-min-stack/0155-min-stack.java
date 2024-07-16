a36d138d8704da96964cfcb38ac3eef5fae49a3a
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-07-16 14:01:37 +0530

    Time: 4 ms (95.64%), Space: 44.5 MB (85.44%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,32 +1,82 @@
+// class MinStack {
+// Stack<Integer> st=new Stack<>();
+//     public MinStack() {
+        
+//     }
+    
+//     public void push(int val) {
+        
+//             st.push(val);
+        
+//     }
+    
+//     public void pop() {
+//         // if(!st.isEmpty()){
+//             st.pop();
+//         // }
+//     }
+    
+//     public int top() {
+//           // if(st.isEmpty()) return 0;
+        
+//      return st.peek();
+//     }
+    
+//     public int getMin() {
+    
+        
+//            // return st.peek();
+//           return Collections.min(st);
+//     }
+// }
+
+
+
+
+
 class MinStack {
-Stack<Integer> st=new Stack<>();
+    Stack < Long > st = new Stack < Long > ();
+    Long mini;
+
+    /** initialize your data structure here. */
     public MinStack() {
-        
+        mini = Long.MAX_VALUE;
     }
-    
-    public void push(int val) {
-        
+
+    public void push(int value) {
+        Long val = Long.valueOf(value);
+        if (st.isEmpty()) {
+            mini = val;
             st.push(val);
-        
+        } else {
+            if (val < mini) {
+                st.push(2 * val - mini);
+                mini = val;
+            } else {
+                st.push(val);
+            }
+        }
     }
-    
+
     public void pop() {
-        // if(!st.isEmpty()){
-            st.pop();
-        // }
+        if (st.isEmpty()) return;
+
+        Long val = st.pop();
+        if (val < mini) {
+            mini = 2 * mini - val;
+        }
     }
-    
+
     public int top() {
-          // if(st.isEmpty()) return 0;
-        
-     return st.peek();
+        Long val = st.peek();
+        if (val < mini) {
+            return mini.intValue();
+        }
+        return val.intValue();
     }
-    
+
     public int getMin() {
-    
-        
-           // return st.peek();
-          return Collections.min(st);
+        return mini.intValue();
     }
 }
 
