416fafa1e8dce672aea0e992e61598cf535d8b5c
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-07-22 12:47:39 +0530

    Time: 0 ms (100.00%), Space: 44.7 MB (44.95%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,24 +14,41 @@
  * }
  */
 class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         if(root == null) return 0;
         
-        int dia1=diameterOfBinaryTree(root.left);
-        int dia2=diameterOfBinaryTree(root.right);
+//         int dia1=diameterOfBinaryTree(root.left);
+//         int dia2=diameterOfBinaryTree(root.right);
         
-        int dia3= height(root.left)+height(root.right);
+//         int dia3= height(root.left)+height(root.right);
         
-        return Math.max(dia3,Math.max(dia1,dia2));
+//         return Math.max(dia3,Math.max(dia1,dia2));
+//     }
+    
+    
+//     public int height(TreeNode root){
+//         if(root==null)return 0;
+        
+//         int lefth=height(root.left);
+//         int righth=height(root.right);
+        
+//         return Math.max(lefth,righth)+1;
+//     }
+    
+    
+    // o(n)
+      int res = 0;
+
+    public int height(TreeNode root) {
+        if (root == null) return 0;
+        int lh = height(root.left);
+        int rh = height(root.right);
+        res = Math.max(res, lh + rh);
+        return 1 + Math.max(lh, rh);
     }
-    
-    
-    public int height(TreeNode root){
-        if(root==null)return 0;
-        
-        int lefth=height(root.left);
-        int righth=height(root.right);
-        
-        return Math.max(lefth,righth)+1;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return res;
     }
 }
